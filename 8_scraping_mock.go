67ff35d67624154ef13cb64d4bb059c0c7c51950
yoshi42662 <yoshihiro_tkhs@yahoo.co.jp>
2017-12-21 23:58:34 +0900

    refactor scraping mock

--- 8_scraping_mock.go
+++ 8_scraping_mock.go
@@ -8,55 +8,35 @@ import (
 	"time"
 )
 
-func main() {
-	// runtime側の並列数を設定
-	runtime.GOMAXPROCS(runtime.NumCPU() * 2)
-	// runtime.GOMAXPROCS(runtime.NumCPU() * 2)
-
-	// 開始時間を記録
-	start := time.Now()
-
-	var wg sync.WaitGroup
-
-	q := make(chan int)
-
+var (
 	// CPUのコア数を取得
-	cpuCount := runtime.NumCPU()
-	fmt.Println("CPU count is " + fmt.Sprintf("%d", cpuCount))
+	cpuCount = runtime.NumCPU()
 
-	// ワーカー数の設定
-	workerCount := cpuCount * 2
-	// workerCount = 1
+	// runtime側の並列数
+	maxProcsCount = cpuCount * 2
 
-	// ワーカーの作成
-	for i := 0; i < workerCount; i++ {
-		wg.Add(1)
-		go fetchURL(&wg, q)
-	}
+	// goroutinのworker数
+	// workerCount = cpuCount * 16
+	workerCount = 1
 
 	// 処理したいタスクの数を設定
-	taskCount := 100
+	taskCount = 100
+)
 
-	// workerCountの値が同時処理数でタスクを処理する
-	for index := 0; index < taskCount; index++ {
-		fmt.Println("Queueing : ", index)
-		q <- index
-	}
-
-	// channelでの処理の最後は必ずchannelをcloseする
-	close(q)
-
-	// すべてのgoroutineが終了するのをまつ
-	wg.Wait()
-
-	// 終了時間を計測
-	end := time.Now()
-
-	// 実行時間を算出
-	fmt.Printf("%f秒\n", (end.Sub(start)).Seconds())
+func init() {
+	// runtime側の並列数を設定
+	runtime.GOMAXPROCS(maxProcsCount)
 }
 
-func fetchURL(wg *sync.WaitGroup, q chan int) {
+// 0ms ~ 200msの間でレスポンスを返す
+func mockAPIServer() bool {
+	time.Sleep(time.Duration(rand.Intn(200)) * time.Millisecond)
+
+	return true
+}
+
+// 擬似スクレイピング用の関数
+func mockFetchURL(wg *sync.WaitGroup, q chan int) {
 	// defer文はfetchURL関数のブロックを抜ける際に実行される
 	defer wg.Done()
 
@@ -75,11 +55,38 @@ func fetchURL(wg *sync.WaitGroup, q chan int) {
 	}
 }
 
-func mockAPIServer() bool {
-	// Do something a bit heavy
+func main() {
+	// 開始時間を記録
+	start := time.Now()
 
-	// 0ms ~ 200msの間でレスポンスを返す
-	time.Sleep(time.Duration(rand.Intn(200)) * time.Millisecond)
+	var wg sync.WaitGroup
 
-	return true
+	// int型のchannelを用意
+	q := make(chan int)
+
+	// goroutin workerの生成
+	for i := 0; i < workerCount; i++ {
+		wg.Add(1)
+		go mockFetchURL(&wg, q)
+	}
+
+	// workerCountの値が同時処理数でタスクを処理する
+	for index := 0; index < taskCount; index++ {
+		fmt.Println("Queueing : ", index)
+
+		// indexをqに送信
+		q <- index
+	}
+
+	// channelでの処理の最後は必ずchannelをcloseする
+	close(q)
+
+	// すべてのgoroutineが終了するのをまつ
+	wg.Wait()
+
+	// 終了時間を計測
+	end := time.Now()
+
+	// 実行時間を算出
+	fmt.Printf("\n\n実行時間: %f秒\n\n", (end.Sub(start)).Seconds())
 }
